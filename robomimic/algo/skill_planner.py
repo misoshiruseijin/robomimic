9163c641b219d373c4c587771556ae0ba3f0a71b
Minjune Hwang <mjhwang@berkeley.edu>
2023-08-09 13:52:57 -0700

    add skill planner to algo

---
+++ robomimic/algo/skill_planner.py
@@ -0,0 +1,235 @@
+"""
+Subgoal prediction models, used in HBC / IRIS.
+"""
+import numpy as np
+from collections import OrderedDict
+from copy import deepcopy
+
+import torch
+import torch.nn as nn
+
+import robomimic.models.obs_nets as ObsNets
+import robomimic.models.vae_nets as VAENets
+import robomimic.utils.tensor_utils as TensorUtils
+import robomimic.utils.torch_utils as TorchUtils
+import robomimic.utils.obs_utils as ObsUtils
+
+from robomimic.algo import register_algo_factory_func, PlannerAlgo, ValueAlgo
+
+
+@register_algo_factory_func("skill_planner")
+def algo_config_to_class(algo_config):
+    """
+    Maps algo config to the SkillPlanner algo class to instantiate, along with additional algo kwargs.
+
+    Args:
+        algo_config (Config instance): algo config
+
+    Returns:
+        algo_class: subclass of Algo
+        algo_kwargs (dict): dictionary of additional kwargs to pass to algorithm
+    """
+    return SkillPlanner, {}
+
+
+class SkillPlanner(PlannerAlgo):
+    """
+    Implements skill planner.
+    """
+    def __init__(
+        self,
+        algo_config,
+        obs_config,
+        global_config,
+        obs_key_shapes,
+        ac_dim,
+        device
+    ):
+        """
+        Args:
+            algo_config (Config object): instance of Config corresponding to the algo section
+                of the config
+
+            obs_config (Config object): instance of Config corresponding to the observation
+                section of the config
+
+            global_config (Config object): global training config
+
+            obs_key_shapes (OrderedDict): dictionary that maps observation keys to shapes
+
+            ac_dim (int): dimension of action space
+
+            device (torch.Device): where the algo should live (i.e. cpu, gpu)
+        """
+
+        self.num_skills = algo_config.num_skills
+        super(SkillPlanner, self).__init__(
+            algo_config=algo_config,
+            obs_config=obs_config,
+            global_config=global_config,
+            obs_key_shapes=obs_key_shapes,
+            ac_dim=ac_dim,
+            device=device
+        )
+
+    def _create_networks(self):
+        """
+        Creates networks and places them into @self.nets.
+        """
+        self.nets = nn.ModuleDict()
+
+        obs_group_shapes = OrderedDict()
+        obs_group_shapes["obs"] = OrderedDict(self.obs_shapes)
+        if len(self.goal_shapes) > 0:
+            obs_group_shapes["goal"] = OrderedDict(self.goal_shapes)
+
+        # deterministic goal prediction network
+        self.nets["policy"] = ObsNets.MIMO_MLP(
+            input_obs_group_shapes=obs_group_shapes, 
+            output_shapes=OrderedDict(skill=(self.num_skills,)),
+            layer_dims=self.algo_config.actor_layer_dims,
+            encoder_kwargs=ObsUtils.obs_encoder_kwargs_from_config(self.obs_config.encoder),
+        )
+
+        self.nets = self.nets.float().to(self.device)
+
+    def process_batch_for_training(self, batch):
+        """
+        Processes input batch from a data loader to filter out
+        relevant information and prepare the batch for training.
+
+        Args:
+            batch (dict): dictionary with torch.Tensors sampled
+                from a data loader
+
+        Returns:
+            input_batch (dict): processed and filtered batch that
+                will be used for training 
+        """
+        input_batch = dict()
+
+        # remove temporal batches for all except scalar signals (to be compatible with model outputs)
+        input_batch["obs"] = {k: batch["obs"][k][:, 0, :] for k in batch["obs"]}
+        input_batch["goal_obs"] = batch.get("goal_obs", None) # goals may not be present
+        input_batch["skills"] = {k: batch["skills"][k][:, 0, :] for k in batch["obs"]}
+
+        # we move to device first before float conversion because image observation modalities will be uint8 -
+        # this minimizes the amount of data transferred to GPU
+        return TensorUtils.to_float(TensorUtils.to_device(input_batch, self.device))
+
+    def train_on_batch(self, batch, epoch, validate=False):
+        """
+        Training on a single batch of data.
+
+        Args:
+            batch (dict): dictionary with torch.Tensors sampled
+                from a data loader and filtered by @process_batch_for_training
+
+            epoch (int): epoch number - required by some Algos that need
+                to perform staged training and early stopping
+
+            validate (bool): if True, don't perform any learning updates.
+
+        Returns:
+            info (dict): dictionary of relevant inputs, outputs, and losses
+                that might be relevant for logging
+        """
+        with TorchUtils.maybe_no_grad(no_grad=validate):
+            info = super(SkillPlanner, self).train_on_batch(batch, epoch, validate=validate)
+            predictions = self._forward_training(batch)
+            losses = self._compute_losses(predictions, batch)
+
+            info["predictions"] = TensorUtils.detach(predictions)
+            info["losses"] = TensorUtils.detach(losses)
+
+            if not validate:
+                step_info = self._train_step(losses)
+                info.update(step_info)
+
+        return info
+
+    def _forward_training(self, batch):
+        """
+        Internal helper function for BC algo class. Compute forward pass
+        and return network outputs in @predictions dict.
+
+        Args:
+            batch (dict): dictionary with torch.Tensors sampled
+                from a data loader and filtered by @process_batch_for_training
+
+        Returns:
+            predictions (dict): dictionary containing network outputs
+        """
+        predictions = OrderedDict()
+        skill_logits = self.nets["policy"](obs_dict=batch["obs"], goal_dict=batch["goal_obs"])["skill"]
+        predictions["skill_logits"] = skill_logits
+        return predictions
+        
+    def _compute_losses(self, predictions, batch):
+        """
+        Internal helper function for BC algo class. Compute losses based on
+        network outputs in @predictions dict, using reference labels in @batch.
+
+        Args:
+            predictions (dict): dictionary containing network outputs, from @_forward_training
+            batch (dict): dictionary with torch.Tensors sampled
+                from a data loader and filtered by @process_batch_for_training
+
+        Returns:
+            losses (dict): dictionary of losses computed over the batch
+        """
+        losses = OrderedDict()
+        skill_logits = predictions["actions"]
+        skill_targets = batch["actions"]
+        losses["skill_classification_loss"] = nn.CrossEngropy()(skill_logits, skill_targets)
+        return losses
+
+    def _train_step(self, losses):
+        """
+        Internal helper function for BC algo class. Perform backpropagation on the
+        loss tensors in @losses to update networks.
+
+        Args:
+            losses (dict): dictionary of losses computed over the batch, from @_compute_losses
+        """
+
+        # gradient step
+        info = OrderedDict()
+        policy_grad_norms = TorchUtils.backprop_for_loss(
+            net=self.nets["policy"],
+            optim=self.optimizers["policy"],
+            loss=losses["skill_classification_loss"],
+        )
+        info["policy_grad_norms"] = policy_grad_norms
+        return info
+
+    def log_info(self, info):
+        """
+        Process info dictionary from @train_on_batch to summarize
+        information to pass to tensorboard for logging.
+
+        Args:
+            info (dict): dictionary of info
+
+        Returns:
+            loss_log (dict): name -> summary statistic
+        """
+        loss_log = super(SkillPlanner, self).log_info(info)
+
+        loss_log["Loss"] = info["losses"]["skill_classification_loss"].item()
+
+        return loss_log
+
+    def get_action(self, obs_dict, goal_dict=None):
+        """
+        Get policy action outputs.
+
+        Args:
+            obs_dict (dict): current observation
+            goal_dict (dict): (optional) goal
+
+        Returns:
+            action (torch.Tensor): action tensor
+        """
+        assert not self.nets.training
+        return self.nets["policy"](obs_dict, goal_dict=goal_dict)
